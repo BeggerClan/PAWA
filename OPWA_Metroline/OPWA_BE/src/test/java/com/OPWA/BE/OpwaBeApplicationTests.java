92c2bdef0d6d8137b074d035f230ff3f2f1ac39c
dattran982 <dattran7498@gmail.com>
2025-04-15 10:31:29 +0700

    Add files via upload

---
+++ OPWA_Metroline/OPWA_BE/src/test/java/com/OPWA/BE/OpwaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OPWA.BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpwaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
