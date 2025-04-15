a8229b1fa2cc967f3eaec4d5b7c7bb4a7bf37de4
dattran982 <dattran7498@gmail.com>
2025-04-15 11:03:17 +0700

    setup

---
+++ opwa_be/src/main/java/com/opwa/opwa_be/OpwaBeApplication.java
@@ -0,0 +1,13 @@
+package com.opwa.opwa_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpwaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpwaBeApplication.class, args);
+	}
+
+}
---
+++ opwa_be/src/test/java/com/opwa/opwa_be/OpwaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opwa.opwa_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpwaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
