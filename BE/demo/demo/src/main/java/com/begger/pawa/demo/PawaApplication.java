257e2b6fd847c109cb07c52d01edd6b50cd2dce2
darmondcv <155642847+darmondcv@users.noreply.github.com>
2025-04-21 13:37:27 +0700

    Import Springboot Project
    Import Springboot Project

---
+++ BE/demo/demo/src/main/java/com/begger/pawa/demo/PawaApplication.java
@@ -0,0 +1,13 @@
+package com.begger.pawa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawaApplication.class, args);
+	}
+
+}
